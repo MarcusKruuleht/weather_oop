1a286c8a0bd156cda89aedba35a32f84d1d0638a
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2021-11-12 15:00:01 +0200

    weather expanded and enhanced

--- classes/UI.js
+++ classes/UI.js
@@ -3,6 +3,12 @@ class UI {
 		this.description = document.querySelector('#description')
 		this.temp = document.querySelector('#temp')
 		this.location = document.querySelector('#location')
+		this.windspeed = document.querySelector('#windspeed')
+		this.winddirection = document.querySelector('#winddirection')
+		this.pressure = document.querySelector('#pressure')
+		this.humidity = document.querySelector('#humidity')
+
+
 	}
 
 	drawWeather( data ) {
@@ -12,5 +18,10 @@ class UI {
 	this.description.innerHTML = description;
 	this.temp.innerHTML = celsius + '&deg;';
 	this.location.innerHTML = data.name;
+	this.windspeed.innerHTML = data.wind.speed + ' m/s';
+	this.winddirection.innerHTML = data.wind.deg + '&deg Tuule suund';
+	this.pressure.innerHTML = data.main.pressure + ' mmHg';
+	this.humidity.innerHTML = data.main.humidity + ' %';
+
 }
 }
