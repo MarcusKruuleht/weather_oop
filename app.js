9f812333e73eab6bbe0fe91979c272cfe2f92d68
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2021-11-12 14:09:46 +0200

    set and get weathe rlocation from LS

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+// LS object
+const ls = new LS()
+console.log(ls)
+// init location data
+const cityName = ls.getLocationData()
+console.log(cityName)
+
 // Weather object
-const weather = new Weather('Tallinn')
+const weather = new Weather(cityName)
 
 // UI object
 const ui = new UI()
 console.log(ui)
+
+// city changing
+const form = document.querySelector('#change-city')
+form.addEventListener('submit', changeCityWeather)
+
+// change city weather
+function changeCityWeather(e){
+	const cityName = document.querySelector('#city-name').value
+	console.log(cityName)
+	weather.changeCity(cityName)
+	ls.setLocationData(cityName)
+	getCityWeather()
+	document.querySelector('#city-name').value=''
+	e.preventDefault()
+}
+
 
 // get city weather and display it
 function getCityWeather(){
